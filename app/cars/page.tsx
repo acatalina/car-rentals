1aa02783599e2621143e87bfbc1d7c364e51d841
Alvaro Catalina <accarrasco@gmail.com>
2024-08-20 10:21:43 +0100

    sorting cars by price

--- app/cars/page.tsx
+++ app/cars/page.tsx
@@ -4,11 +4,26 @@ import CarCard from '@/components/CarCard';
 import CarLogoBanner from '@/components/CarLogoBanner';
 import { carsForRent } from '@/data';
 import { useCallback, useEffect, useState } from 'react';
-// @ts-ignore
-import useEmblaCarousel, { EmblaCarouselType } from 'embla-carousel-react';
+import useEmblaCarousel from 'embla-carousel-react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { Button } from '../../components/ui/button';
+
+type SortingType = "price-asc" | "price-desc";
+
+function getCars(sortType: SortingType) {
+	if (sortType === "price-desc") {
+		return carsForRent.slice().sort((a, b) => a.price - b.price);
+	} else {
+		return carsForRent.slice().sort((a, b) => b.price - a.price);
+	}
+}
+
+export function getSortText(sortType: SortingType) {
+	return sortType === "price-asc" ? "Sort by Price (Low to High)" : "Sort by Price (High to Low)"
+}
 
 const Cars = () => {
+	const [sortType, setSortType] = useState<SortingType>("price-asc");
 	const [emblaRef, emblaApi] = useEmblaCarousel({
 		align: 'start',
 		loop: false,
@@ -19,6 +34,8 @@ const Cars = () => {
 			'(min-width: 768px)': { slidesToScroll: 2 },
 		},
 	});
+
+	const cars = getCars(sortType);
 
 	const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
 	const [nextBtnEnabled, setNextBtnEnabled] = useState(true);
@@ -32,7 +49,7 @@ const Cars = () => {
 		[emblaApi]
 	);
 
-	const onSelect = useCallback((api: EmblaCarouselType) => {
+	const onSelect = useCallback((api: NonNullable<typeof emblaApi>) => {
 		setPrevBtnEnabled(api.canScrollPrev());
 		setNextBtnEnabled(api.canScrollNext());
 	}, []);
@@ -58,13 +75,25 @@ const Cars = () => {
 		};
 	}, [emblaApi, onSelect]);
 
+	const handleOnClick = () => {
+		if (sortType === "price-asc") {
+			setSortType("price-desc");
+		} else {
+			setSortType("price-asc");
+		}
+	};
+	const sortButton = getSortText(sortType);
+
 	return (
 		<main className='md:overflow-y-hidden max-h-[87vh]'>
 			<div className='mt-8 md:mt-10 lg:mt-12'>
 				<CarLogoBanner />
 			</div>
+			<Button onClick={handleOnClick}>
+				{sortButton}
+			</Button>
 			<div className='grid sm:hidden grid-cols-1 gap-6 mt-6 pb-12 md:pb-0'>
-				{carsForRent.map(car => (
+				{cars.map(car => (
 					<CarCard
 						key={car.vehicleId}
 						image={car.image}
@@ -87,7 +116,7 @@ const Cars = () => {
 					ref={emblaRef}
 				>
 					<div className='flex'>
-						{carsForRent.map(car => (
+						{cars.map(car => (
 							<div
 								key={car.vehicleId}
 								className='flex-[0_0_50%] lg:flex-[0_0_33.333%] sm:max-h-[465px] min-w-0 pl-4 pr-4'
